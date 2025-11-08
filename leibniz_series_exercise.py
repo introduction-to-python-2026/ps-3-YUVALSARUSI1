bd700de55c03a81894bd0736106a8269df44b445
YUVALSARUSI1 <yuval4.2000@gmail.com>
2025-11-08 22:51:12 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,7 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+  result2 = 0
+  for n in range(n_terms):
+   result1 = ((-1)**n)/(2*n+1)
+   result2 += result1
+
+  return 4*result2
