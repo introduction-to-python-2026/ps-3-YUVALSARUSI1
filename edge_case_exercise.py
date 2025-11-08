c4e3a2157e49a77e3ed71a3bd994447ae47cd44f
YUVALSARUSI1 <yuval4.2000@gmail.com>
2025-11-08 22:20:00 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,18 +1,1 @@
-def move(my_list, direction):
-  one = my_list.index(1)
-  lengh = len(my_list)
-  if direction == "right":
-    if one == lengh - 1:
-      return my_list
-    else:
-      my_list[one]=0
-      my_list[one+1]=1
-      return my_list
-  elif direction == "left":
-    if one ==0:
-      return my_list
-    else:
-      my_list[one]=0
-      my_list[one-1]=1
-      return my_list
 
